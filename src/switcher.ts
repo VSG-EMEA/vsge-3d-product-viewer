dffd9a0594bd34b440012a6cd0bce0503813b867
Erik Golinelli <erik@codekraft.it>
2023-02-16 11:46:20 +0100

    updates to ml 3.0.1

--- src/switcher.ts
+++ src/switcher.ts
@@ -1,4 +1,8 @@
 /**
+ * True if you want the 3D model to be displayed immediately, if not the gallery will be displayed first
+ */
+
+/**
  * WOOCOMMERCE MEDIA SECTION GALLERY
  * It adds an event listener to each button in the gallery switcher, and when clicked, it toggles the
  * visibility of the 3D model viewer
--- vsge-3d-product-viewer/build/vsge-3d-product-viewer.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'c5b564cccd4952bdf04b');
