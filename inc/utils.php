f6a4cf3f552af4b04a468783cc1efd23a9170c19
Erik Golinelli <erik@codekraft.it>
2024-02-29 23:20:04 +0100

    add the utility function file adds a function that return if the current post has a 3d model

---
+++ inc/utils.php
@@ -0,0 +1,43 @@
+<?php
+
+
+/**
+ * If the product has a 3D model, add a class to the product gallery container
+ *
+ * @param array $classes classes Array of CSS classes to add to the container.
+ *
+ * @return array the new product classes array.
+ */
+function vsge_3d_model_container_class( $classes ) {
+	global $post;
+	if ($post && is_product()) {
+		$classes[] = has_3d_model()K
+			? 'woocommerce-product-gallery--with-vr'
+			: 'woocommerce-product-gallery--without-vr ';
+	}
+
+	return $classes;
+}
+
+/**
+ * Returns true if the browser is safari.
+ *
+ * @param string $user_agent The user agent.
+ *
+ * @return bool true if the browser is safari, false otherwise.
+ */
+function vsge_3d_model_is_safari( $user_agent ) {
+	if ( strlen( strstr( $user_agent, 'iPhone' ) ) > 0 || strlen( strstr( $user_agent, 'iPad' ) ) > 0 ) {
+		return true;
+	}
+}
+
+/**
+ * Check if the post has a 3D model attached.
+ *
+ * @return bool true if the post has a 3D model, false otherwise.
+ */
+function has_3d_model() {
+	global $post;
+	return get_post_meta( $post->ID, VSGE_MV_PLUGIN_NAMESPACE . '_media_3d_model', true );
+}
