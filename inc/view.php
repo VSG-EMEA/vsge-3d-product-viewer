87f0109356a5d121249a1e62b7ea2dda1d63b403
Erik Golinelli <erik@codekraft.it>
2024-03-01 22:36:53 +0100

    update redirect in order to allow url like "domain.com/model3d=product-slug"

--- inc/view.php
+++ inc/view.php
@@ -6,13 +6,22 @@
  */
 function vsge_3d_model_redirect( $template ) {
 	// get the requested page and the query string
-	$page = explode( '/', $_SERVER['REQUEST_URI'] );
-	if ( ! empty( $page[1] ) ) {
-		// get the current page query string and check if it matches with model3d
-		$query_string =  explode( "=", $page[2] ) ;
-		if ($query_string[0] === 'model3d?id') {
+	$page = explode( '/', rtrim($_SERVER['REQUEST_URI'], '?') );
+	$model = explode( "=", sanitize_text_field( $page[ count( $page ) - 1 ] ) );
+
+	// check if the requested page is a 3D model
+	if ( ! empty( $model[0] )  ) {
+		$string_slug = $model[0];
+		$model = $model[1];
+		if ($string_slug === 'model3d') {
+			if ( ! is_numeric( $model ) ) {
+				$product_id = get_page_by_path( sanitize_text_field($model), OBJECT, 'product' )->ID;
+			} else {
+				$product_id = intval($model);
+			}
+
 			// check if the post has a 3D model
-			if ( has_3d_model( $query_string[1] ) ) {
+			if ( has_3d_model($product_id ) ) {
 				// check if the post has a 3D model
 				$template = VSGE_MV_PLUGIN_DIR . '/template/single-3d-model.php';
 			} else {
@@ -20,6 +29,7 @@ function vsge_3d_model_redirect( $template ) {
 				$template = VSGE_MV_PLUGIN_DIR . '/template/missing-3d-model.php';
 			}
 		}
+
 	}
 
 	return $template;
