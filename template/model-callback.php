5f5823e7a0783dc32f1399707a5bf9a74923b852
Erik Golinelli <erik@codekraft.it>
2024-03-01 13:32:40 +0100

    template page model-callback.php (used in the woo product page)

--- template/model-callback.php
+++ template/model-callback.php
@@ -2,6 +2,59 @@
 /**
  * Template for the 3D model viewer.
  */
+global $post;
+
+$model_3d = get_post_meta( $post->ID, VSGE_MV_PLUGIN_NAMESPACE . '_media_3d_model', true );
+
+if ( ! $model_3d ) {
+	return;
+}
+
+$model_url     = wp_get_attachment_url( $model_3d );
+$model_preview = get_post_meta( $model_3d, VSGE_MV_PLUGIN_NAMESPACE . '_media_3d_model_preview', true );
+
+$model_data = (array) json_decode( get_post_meta( $model_3d, VSGE_MV_PLUGIN_NAMESPACE . '_media_3d_model_data', true ) );
+
+$model_options = implode(
+	' ',
+	array(
+		isset( $model_data['camera-orbit'] ) && $model_data['camera-orbit'] !== '65deg 90deg 25m' ? ' camera-orbit="' . $model_data['camera-orbit'] . '"' : null,
+		isset( $model_data['camera-target'] ) && $model_data['camera-target'] !== '0m 4.5m 0m'  ? ' camera-target="' . $model_data['camera-target'] . '"' : null,
+	)
+);
+
+// if the hotspots have set generate the hotspot HTML pointers
+$hotspots_html = '';
+if ( ! empty( $model_data['hotspots'] ) ) {
+	foreach ( $model_data['hotspots'] as $k => $hotspot ) {
+		if ( isset( $hotspot->position, $hotspot->title ) ) {
+			$slot = $hotspot->slot ? $hotspot->slot : 'hotspot-' . $k;
+			if ( isset( $hotspot->href ) ) {
+				$hotspots_html .= sprintf(
+					'<div class="hotspot" slot="hotspot-%s" data-position="%s" data-visibility-attribute="visible">
+          <div class="dot"></div>
+          <a class="annotation" href="%s">%s</a>
+        </div>',
+					$slot,
+					$hotspot->position,
+					$hotspot->href,
+					$hotspot->title
+				);
+			} else {
+				$hotspots_html .= sprintf(
+					'<div class="hotspot" slot="hotspot-%s" data-position="%s" data-visibility-attribute="visible">
+          <div class="dot"></div>
+          <span class="annotation">%s</span>
+        </div>
+',
+					$slot,
+					$hotspot->position,
+					$hotspot->title
+				);
+			}
+		}
+	}
+}
 ?>
 <div id="woocommerce-product-gallery__3d" style="display:none">
 	<div class="model-viewer-helpers">
@@ -20,10 +73,15 @@
 					<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m357-167-43-43 80-81q-136-15-225-66T80-486q0-79 116.5-134.5T480-676q168 0 284 55.5T880-486q0 59-64 104t-170 70v-65q80-20 127-52t47-57q0-32-83.5-81T480-616q-172 0-256 49t-84 81q0 45 57.5 77.5T397-349l-83-81 43-43 153 152-153 154Z"/></svg>
 				</span>
 		</button>
+		<button slot="ar-info" id="ar-info" class="active">
+				<span class="button-icon">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><g fill="none" stroke="#000" data-name="Icon akar-info"><path d="M27 14A13 13 0 1 1 14 1a13 13 0 0 1 13 13Z" data-name="Tracciato 4148"/><path stroke-linecap="round" stroke-width="3" d="M14 8h0" data-name="Tracciato 4149"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 13h3v6m-3 0h5" data-name="Tracciato 4150"/></g></svg>
+				</span>
+		</button>
 		<?php if ( ! empty( $hotspots_html ) ) { ?>
 			<button slot="ar-toggle-hotspots" id="ar-toggle-hotspots" class="active">
 					<span class="button-icon">
-						<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M480-120 300-300l44-44 136 136 136-136 44 44-180 180ZM344-612l-44-44 180-180 180 180-44 44-136-136-136 136Z"/></svg>
+						<svg xmlns="http://www.w3.org/2000/svg" height="36" width="36" viewBox="0 -960 960 960"><path d="M480-120 300-300l44-44 136 136 136-136 44 44-180 180ZM344-612l-44-44 180-180 180 180-44 44-136-136-136 136Z"/></svg>
 					</span>
 			</button>
 		<?php } ?>
@@ -54,11 +112,17 @@
 	<button id="woo-select-3d" data-type="3d-model"><?php esc_html_e( '3D/VR', 'vsge-3d-product-viewer' ); ?></button>
 	<button id="woo-select-gallery" class="active" data-type="gallery"><?php esc_html_e( 'Gallery', 'vsge-3d-product-viewer' ); ?></button>
 </div>
-<div id="vsge-modal-qrcode" class="vsge-modal-notice outer-modal">
+<div id="vsge-modal-3d" class="vsge-modal-notice outer-modal">
 	<div class="inner-modal">
-		<canvas id="vsge-vr-model"></canvas>
-		<h3><?php esc_html_e( 'Instructions:', 'vsge-3d-product-viewer' ); ?></h3>
-		<p><?php esc_html_e( 'Scan this code to open the model on your device, then, tap on the AR icon.', 'vsge-3d-product-viewer' ); ?></p>
-		<p><b><?php esc_html_e( 'Click to close this message', 'vsge-3d-product-viewer' ); ?></b></p>
+		<div class="vsge-modal-qr">
+			<canvas id="vsge-vr-model"></canvas>
+			<h3><?php esc_html_e( 'Instructions:', 'vsge-3d-product-viewer' ); ?></h3>
+			<p><?php esc_html_e( 'Scan this code to open the model on your device, then, tap on the AR icon.', 'vsge-3d-product-viewer' ); ?></p>
+			<p><b><?php esc_html_e( 'Click to close this message', 'vsge-3d-product-viewer' ); ?></b></p>
+		</div>
+
+		<div class="vsge-modal-info">
+			<h3><?php esc_html_e( 'info:', 'vsge-3d-product-viewer' ); ?></h3>
+		</div>
 	</div>
 </div>
