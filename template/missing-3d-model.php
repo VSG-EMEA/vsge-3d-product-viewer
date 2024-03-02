f564531c68be049ed917e90bf1df124de527bf39
Erik Golinelli <erik@codekraft.it>
2024-03-02 18:15:08 +0100

    chore

--- template/missing-3d-model.php
+++ template/missing-3d-model.php
@@ -1,1 +1,3 @@
-<p><?php esc_html_e( 'This product does not have a 3D model yet', 'vsge-3d-product-viewer' ); ?></p>
+<div class="mv-wrapper-error">
+	<p><?php esc_html_e( 'This product does not have a 3D model yet', 'vsge-3d-product-viewer' ); ?></p>
+</div>
--- vsge-3d-product-viewer.php
+++ vsge-3d-product-viewer.php
@@ -33,12 +33,12 @@ include_once VSGE_MV_PLUGIN_DIR . '/inc/template-fse.php';
 include_once VSGE_MV_PLUGIN_DIR . '/inc/template-loader.php';
 include_once VSGE_MV_PLUGIN_DIR . '/inc/view.php';
 
-/** Add a custom class to the body whenever the displayed product has a 3D model */
-add_filter( 'woocommerce_single_product_image_gallery_classes', 'vsge_3d_model_container_class' );
-
 /** Load the 3D model viewer script and style */
 add_action( 'wp_enqueue_scripts', 'vsge_mv_frontend_scripts' );
 add_action( 'wp_footer', 'vsge_mv_frontend_style' );
 
 /** Adds to the product gallery container the 3D model viewer */
 add_action( 'woocommerce_after_product-gallery__wrapper', 'vsge_3d_model_container', 20 );
+
+/** Add a custom class to the body whenever the displayed product has a 3D model */
+add_filter( 'woocommerce_single_product_image_gallery_classes', 'vsge_3d_model_container_class' );
